9e86ecbf52a9c97531e420bff73b7bb05eb53e0c
MAC <mac@MACs-MacBook-Pro-2.local>
2025-03-09 06:15:47 +0000

    feat added other users route

--- router/authRouter.js
+++ router/authRouter.js
@@ -11,7 +11,7 @@ const router = Router();
 router.post("/signUp", signUp);
 router.get("/get-all-users", getAllUsers);
 router.get("/get-a-user/:id", getAllUsers);
-router.patch("/get-a-user/:id", updateUser);
-router.delete("/get-a-user/:id", deleteUser);
+router.patch("/update-a-user/:id", updateUser);
+router.delete("/delete-a-user/:id", deleteUser);
 
 export const authRouter = router;
